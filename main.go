b38d0c99b074bca285576fdcbc1dad0531141fe0
Dmitry Goncharov <devgda@mail.ru>
2019-07-04 00:46:44 +0500

    Начал проработывать оружие его выбор, хар-ки Рандомно назначать если пользователь ничего не выбрал Добавил функцию для выдачи рандомных значений

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ type Players interface {
 	getDamage(damage int)
 }
 
-
 type mainMenuText struct {
 	welcome string
 	start   string
@@ -23,15 +22,21 @@ type mainMenuText struct {
 type Hero struct {
 	name   string
 	health int
-	weapon string
+	weapon weapon
 	damage float32
 }
 
 type Dragon struct {
 	name   string
 	health int
-	weapon string
+	weapon weapon
 	damage float32
+}
+
+type weapon struct {
+	name      string
+	minDamage int
+	maxDamage int
 }
 
 func (hero *Hero) getDamage(damage int) {
@@ -43,7 +48,7 @@ func (hero *Hero) setName(name string) {
 }
 
 func (dragon *Dragon) getDamage(damage int) {
-	dragon.health -= damage;
+	dragon.health -= damage
 }
 
 func (dragon *Dragon) setName(name string) {
@@ -54,8 +59,13 @@ func setName(players Players, name string) {
 	players.setName(name)
 }
 
-func hitEnemy(enemy Players, damage int)  {
+func hitEnemy(enemy Players, damage int) {
 	enemy.getDamage(damage)
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Intn(max-min)
 }
 
 func gameStart() {
@@ -90,6 +100,7 @@ func userInput(welcomeLabel string) string {
 func initPlayers() {
 	hero = Hero{
 		name:   userInput("Введите имя игорка:"),
+		weapon: choiceWeapon(initWeapon()),
 		health: 100,
 		damage: 1.0,
 	}
@@ -99,6 +110,7 @@ func initPlayers() {
 	}
 	dragon = Dragon{
 		name:   userInput("Введите имя дракона:"),
+		weapon: choiceWeapon(initWeapon()),
 		health: 100,
 		damage: 1.0,
 	}
@@ -109,17 +121,40 @@ func initPlayers() {
 	fight()
 }
 
-func fight()  {
+func initWeapon() []weapon {
+	weaponList := []weapon{
+		{name: "Меч", minDamage: 10, maxDamage: 20},
+		{name: "Лук", minDamage: 7, maxDamage: 27},
+		{name: "Голые руки", minDamage: 5, maxDamage: 10},
+		{name: "Эскалибур", minDamage: 15, maxDamage: 30},
+		{name: "Перчатка Таноса", minDamage: 30, maxDamage: 50},
+		{name: "Меч джедая", minDamage: 20, maxDamage: 35},
+	}
+	return weaponList
+}
+
+func randChoiceWeapon(weaponList []weapon) weapon {
+	randIndexOfListWeapon := randInt(0, len(weaponList)-1)
+	return weaponList[randIndexOfListWeapon]
+}
+
+func choiceWeapon(weaponList []weapon) weapon {
+	fmt.Println("Выберите оружние")
+	fmt.Println(weaponList)
+	return weaponList[0]
+}
+
+func fight() {
 	fmt.Println("Битва началась")
 	var round = 0
-	for dragon.health > 0 && hero.health >0 {
+	for dragon.health > 0 && hero.health > 0 {
 		fmt.Println("Раунд: ", round)
-		fmt.Printf("%v здоровье: %v\t%v здоровье: %v\n",hero.name, hero.health,dragon.name, dragon.health)
-		hitEnemy(&hero, rand.Intn(10))
-		hitEnemy(&dragon, rand.Intn(10))
+		fmt.Printf("%v здоровье: %v\t%v здоровье: %v\n", hero.name, hero.health, dragon.name, dragon.health)
+		hitEnemy(&hero, randInt(0, 10))
+		hitEnemy(&dragon, randInt(0, 10))
 		round++
 	}
-	fmt.Println("Конец, выиграл ", )
+	fmt.Println("Конец, выиграл ")
 }
 
 var hero = Hero{}
@@ -134,6 +169,5 @@ var menu = mainMenuText{
 }
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
 	gameStart()
 }
